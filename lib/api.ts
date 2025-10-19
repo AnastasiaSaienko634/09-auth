4a59d7f30137130346ec124155155e5aabd87b29
nasti <anastasiasaienko634@gmail.com>
2025-10-19 20:52:18 +0200

    updt

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const nextServer = axios.create({
-  baseURL: "https://09-auth-mocha-mu.vercel.app/",
+  baseURL: "https://09-auth-mocha-mu.vercel.app",
   withCredentials: true,
 });
